48db7b3a1e6eb1af2f16770929ba3492ee5f6981
Luisrds <luisaugusto.social@gmail.com>
2026-08-13 14:36:38 -0300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <header className="site-header">
+      <div className="container header-inner">
+        <Link to="/" className="brand">
+          <span className="brand-small">VEREADORA</span>
+          <strong>ANDREIA</strong>
+          <span>DO JOÃO PAULO II</span>
+        </Link>
+
+        <nav className="nav">
+          <a href="#sobre">Sobre</a>
+          <a href="#atuacao">Atuação</a>
+          <a href="#agenda">Agenda</a>
+          <a href="#gabinete">Gabinete Online</a>
+          <a href="#transparencia">Transparência</a>
+          <a href="#noticias">Notícias</a>
+        </nav>
+
+        <a className="btn btn-primary" href="#gabinete">Fale com a vereadora</a>
+      </div>
+    </header>
+  )
+}
