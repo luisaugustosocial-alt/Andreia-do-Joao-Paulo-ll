b1ef5929136f8cf445d96c4c4d686baea5c88acf
Luisrds <luisaugusto.social@gmail.com>
2026-09-02 15:41:12 -0300

    Ativa animações globais do site

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import './styles/global.css'
 import './styles/overrides.css'
 import './styles/admin.css'
 import './utils/date-dom'
+import './utils/motion'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
