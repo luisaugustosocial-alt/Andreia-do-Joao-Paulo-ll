5de7c26ccd3899b3f856404f2a7d2851a8c8e462
Luisrds <luisaugusto.social@gmail.com>
2026-09-02 14:50:21 -0300

    Adiciona rotas legais

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import Home from './pages/Home'
 import Admin from './pages/Admin'
 import AdminLogin from './pages/AdminLogin'
 import ProtectedRoute from './components/ProtectedRoute'
+import PoliticaPrivacidade from './pages/PoliticaPrivacidade'
+import TermosUso from './pages/TermosUso'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/politica-de-privacidade" element={<PoliticaPrivacidade />} />
+      <Route path="/termos-de-uso" element={<TermosUso />} />
       <Route path="/admin/login" element={<AdminLogin />} />
       <Route
         path="/admin"
