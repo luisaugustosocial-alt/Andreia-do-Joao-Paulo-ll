0dbbf0181939edc128cd23054422bed54b1f63d7
Luisrds <luisaugusto.social@gmail.com>
2026-09-02 14:42:28 -0300

    Adiciona perfil, privacidade e termos

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Admin from './pages/Admin'
 import AdminLogin from './pages/AdminLogin'
+import Perfil from './pages/Perfil'
+import PoliticaPrivacidade from './pages/PoliticaPrivacidade'
+import TermosUso from './pages/TermosUso'
 import ProtectedRoute from './components/ProtectedRoute'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/perfil" element={<Perfil />} />
+      <Route path="/politica-de-privacidade" element={<PoliticaPrivacidade />} />
+      <Route path="/termos-de-uso" element={<TermosUso />} />
       <Route path="/admin/login" element={<AdminLogin />} />
       <Route
         path="/admin"
