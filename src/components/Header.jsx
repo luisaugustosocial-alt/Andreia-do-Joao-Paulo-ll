affe999c9e5d0b295118c363885ce841d6545747
Luisrds <luisaugusto.social@gmail.com>
2026-09-03 13:10:32 -0300

    Encurta chamada do menu para Fale com a vereadora

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ const menuItems = [
   ['/#gabinete', MessageSquareText, 'Gabinete'],
   ['/#transparencia', BarChart3, 'Transparência'],
   ['/#noticias', Newspaper, 'Notícias'],
-  ['/#gabinete', MessageCircle, 'Fale com Andreia']
+  ['/#gabinete', MessageCircle, 'Fale com a vereadora']
 ]
 
 export default function Header() {
